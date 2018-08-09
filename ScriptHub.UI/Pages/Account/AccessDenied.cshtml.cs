9e492625ef4930110fc04e64d0a0f9841b65ce4e
Sebastian L <sebastian.loers@googlemail.com>
2018-08-09 11:05:02 +0200

    initial projects

---
+++ ScriptHub.UI/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ScriptHub.UI.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
